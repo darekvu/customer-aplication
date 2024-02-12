47487c238c8ab33d464850188277471c466cbe85
Quang Du Vu (Darek) <vuquang.dd@gmail.com>
2024-02-12 13:45:20 +0100

    initial commit

---
+++ src/main/java/com/darek/customer/CustomerDataAccessService.java
@@ -0,0 +1,33 @@
+package com.darek.customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CustomerDataAccessService implements CustomerDao {
+    private static final List<Customer> customers;
+
+    static {
+        customers = new ArrayList<>();
+        Customer alex = new Customer(1, "Alex", "alex@gmail.com", 46);
+        Customer kamila = new Customer(2, "Kamila", "kamila@gmail.com", 20);
+        customers.add(alex);
+        customers.add(kamila);
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return customers;
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+         return customers
+                .stream()
+                .filter(customer -> customer.getId().equals(id))
+                .findFirst();
+    }
+}
