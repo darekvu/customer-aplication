a6feda210bb1be92e63180f83f8d0c195b091f5a
Quang Du Vu (Darek) <vuquang.dd@gmail.com>
2024-02-18 16:18:07 +0100

    adding JDBC getAll Customers method

--- src/main/java/com/darek/customer/CustomerJDBCDataAccessService.java
+++ src/main/java/com/darek/customer/CustomerJDBCDataAccessService.java
@@ -1,7 +1,6 @@
 package com.darek.customer;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,9 +9,11 @@ import java.util.Optional;
 @Repository("jdbc")
 public class CustomerJDBCDataAccessService implements CustomerDao {
     private final JdbcTemplate jdbcTemplate;
+    private final CustomerRowMapper customerRowMapper;
 
-    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate) {
+    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate, CustomerRowMapper customerRowMapper) {
         this.jdbcTemplate = jdbcTemplate;
+        this.customerRowMapper = customerRowMapper;
     }
 
     @Override
@@ -20,21 +21,24 @@ public class CustomerJDBCDataAccessService implements CustomerDao {
         var sql = """
                 SELECT * FROM customer;
                 """;
-        RowMapper<Customer> customerRowMapper = (rs, rowNum) -> {
-            Customer customer = new Customer(
-                    rs.getLong("id"),
-                    rs.getString("name"),
-                    rs.getString("email"),
-                    rs.getInt("age")
-            );
-            return customer;
-        };
-        List<Customer> customers = jdbcTemplate.query(sql, customerRowMapper);
-        return customers;
+//       return jdbcTemplate.query(sql,(rs, rowNum) ->{
+//            Customer customer = new Customer(
+//                    rs.getLong("id"),
+//                    rs.getString("name"),
+//                    rs.getString("email"),
+//                    rs.getInt("age")
+//            );
+//            return customer;
+//        });
+        return jdbcTemplate.query(sql,customerRowMapper);
     }
 
     @Override
     public Optional<Customer> selectCustomerById(Long id) {
+//        var sql = """
+//                SELECT * FROM customer
+//                WHERE id = "?"
+//                """;
         return Optional.empty();
     }
 
